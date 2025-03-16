0fcc7c6b83f7e8e3eb081a2ac7f5323185f2bb5c
Katlyn Ferrer <katlynf1@uci.edu>
2025-03-16 06:18:57 +0000

    prompted users for two words in main, finished error function

---
+++ src/ladder_main.cpp
@@ -0,0 +1,11 @@
+#include "ladder.h"
+
+int main() 
+{
+    int word1, word2;
+    cout << "Enter word 1: ";
+    cin >> word1;
+    cout << "\nEnter word 2: ";
+    cin >> word2;
+    //generate_word_ladder(word1, word2);
+}
