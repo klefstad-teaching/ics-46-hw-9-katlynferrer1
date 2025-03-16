523f84275a70b622723fcd7e5086dfbc4d988303
Katlyn Ferrer <katlynf1@uci.edu>
2025-03-16 07:07:36 +0000

    did generatewordladder and made sure to check if words were equal in main

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -2,12 +2,54 @@
 
 void error(string word1, string word2, string msg)
 {
-    cout << word1 << " and " << word2 << " are the same." << endl;
+    cout << word1 << " and " << word2 << ": " << msg << endl;
 }
 
+
+// compares words to see if they are neighbors
 // bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
-// bool is_adjacent(const string& word1, const string& word2);
-// vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
+// {
+
+// }
+
+// examines ladders that are one step away from original word
+// returns true if two words are neighbors (word differs by exactly one letter)
+bool is_adjacent(const string& word1, const string& word2)
+{
+    return true;
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list)
+{
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visited; 
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+
+        for (string word : word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (visited.find(word) == visited.end()) {
+                    visited.insert(word);
+
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word)
+                        return new_ladder;
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    error(begin_word, end_word, "no ladder found");
+    return {};
+}
+
 // void load_words(set<string> & word_list, const string& file_name);
+
 // void print_word_ladder(const vector<string>& ladder);
 // void verify_word_ladder();
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -2,10 +2,14 @@
 
 int main() 
 {
-    int word1, word2;
+    string word1, word2;
     cout << "Enter word 1: ";
     cin >> word1;
     cout << "\nEnter word 2: ";
     cin >> word2;
+    if (word1 == word2) {
+        error(word1, word2, "words are the same");
+        return 0;
+    }
     //generate_word_ladder(word1, word2);
 }
